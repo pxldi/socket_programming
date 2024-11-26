8f4d98b354e659d7850560330c245659e700d9ba
Pxldi <leopold.sappler@gmail.com>
2024-11-26 13:21:18 +0100

    refactor client.py

--- src/client.py
+++ src/client.py
@@ -8,16 +8,9 @@ class CalculationClient:
         self.udp_port = udp_port
 
     def create_request(self, task_id, operation, numbers):
-        # Pack task ID (unsigned int, 4 bytes)
         request = struct.pack('!I', task_id)
-        
-        # Add operation (UTF-8 encoded string)
         request += operation.encode('utf-8')
-        
-        # Pack number of values (unsigned char, 1 byte)
         request += struct.pack('!B', len(numbers))
-        
-        # Pack all numbers (signed int, 4 bytes each)
         for num in numbers:
             request += struct.pack('!i', num)
             
@@ -28,13 +21,10 @@ class CalculationClient:
         
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((self.host, self.tcp_port))
-            
             sock.send(struct.pack('!I', len(request)))
             sock.send(request)
-            
             length = struct.unpack('!I', sock.recv(4))[0]
             response = sock.recv(length)
-            
             task_id, result = struct.unpack('!Ii', response)
             return task_id, result
 
@@ -48,18 +38,22 @@ class CalculationClient:
             task_id, result = struct.unpack('!Ii', response)
             return task_id, result
         
+def test():
+    test_cases = [
+        (1, "SUM", [1, 2, 3, 4, 5]),
+        (2, "PRO", [2, 3, 4]),
+        (3, "MIN", [-5, 0, 5, 10]),
+        (4, "MAX", [1, 100, 50, 25])
+    ]
+
+    for task_id, op, numbers in test_cases:
+        tcp_id, tcp_result = client.send_tcp_request(task_id, op, numbers)
+        print(f"TCP - Task {tcp_id}: {op}({numbers}) = {tcp_result}")
+
+        udp_id, udp_result = client.send_udp_request(task_id, op, numbers)
+        print(f"UDP - Task {udp_id}: {op}({numbers}) = {udp_result}")
+        
 client = CalculationClient()
+# test()
 
-test_cases = [
-    (1, "SUM", [1, 2, 3, 4, 5]),
-    (2, "PRO", [2, 3, 4]),
-    (3, "MIN", [-5, 0, 5, 10]),
-    (4, "MAX", [1, 100, 50, 25])
-]
-
-for task_id, op, numbers in test_cases:
-    tcp_id, tcp_result = client.send_tcp_request(task_id, op, numbers)
-    print(f"TCP - Task {tcp_id}: {op}({numbers}) = {tcp_result}")
-
-    udp_id, udp_result = client.send_udp_request(task_id, op, numbers)
-    print(f"UDP - Task {udp_id}: {op}({numbers}) = {udp_result}")
+tcp_id, tcp_result = client.send_tcp_request(1, "SUM", [1, 1])
